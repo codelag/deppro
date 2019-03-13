c8b8e4018afb8697ae79f649bde104417a232856
Alex Lag <laxeret@gmail.com>
2019-03-13 21:13:40 +0300

    Add base template

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	context = {
+		'title': 'Index page',
 		'name': 'CodeLag',
 	}
 	return render(request, 'pages/index.html', context)
